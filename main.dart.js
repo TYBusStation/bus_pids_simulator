e877a63f6419820a0af88730f30f319080d22564
Myster7494 <Myster7494@users.noreply.github.com>
2026-08-07 01:11:04 +0000

    deploy: 5eb1062e4fdefa0deeb93c3752d29ee9f8ce9c5e

--- main.dart.js
+++ main.dart.js
@@ -41322,7 +41322,7 @@ $S:3}
 A.bwC.prototype={
 $1(a){var s=A.eO().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5a2a6a42cce67f965cf540fcecf616faca624aa1/":s)+a},
 $S:56}
 A.asU.prototype={
 gB(a){var s=this.a
